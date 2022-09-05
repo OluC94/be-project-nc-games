d6e1d60efaaf72398f3755f558f6d346a8316024
OluC94 <coker.olu94@gmail.com>
2022-09-05 14:46:43 +0100

    completes the first status 200 test for GET /api/categories

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -2,6 +2,7 @@ const app = require('../api/app')
 const seed = require('../db/seeds/seed')
 const testData = require('../db/data/test-data/index')
 const db = require('../db/connection');
+const request = require('supertest');
 
 beforeEach(() => {
     return seed(testData);
@@ -9,4 +10,23 @@ beforeEach(() => {
 
 afterAll(() => {
     return db.end();
+})
+
+describe('GET /api/categories', () => {
+    test('200: responds with an array of category objects with the correct properties', () => {
+        console.log('test')
+        return request(app)
+        .get('/api/categories')
+        .expect(200)
+        .then(({body}) => {
+            expect(Array.isArray(body)).toBe(true);
+            expect(body.length > 0).toBe(true);
+            body.forEach((category) => {
+                expect(category).toMatchObject({
+                    slug: expect.any(String),
+                    description: expect.any(String)
+                })
+            })
+        })
+    })
 })
--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { getCategories } = require('./controllers/app.controllers');
 const { handleInternalServerErrors } = require('./controllers/errors.controllers');
 
 const app = express();
 app.use(express.json());
 
+app.get('/api/categories', getCategories);
+
 app.use(handleInternalServerErrors);
 
+module.exports = app;
