af12e1cb1a0a6d02f8f9eb326de64588aeff4fa8
OluC94 <coker.olu94@gmail.com>
2022-09-06 16:16:18 +0100

    completes first 200 test for ticket 7 GET reviews by review ID w/ comment count

--- api/models/app.models.js
+++ api/models/app.models.js
@@ -7,10 +7,30 @@ exports.fetchCategories = () => {
 };
 
 exports.fetchReviewByID = (review_id) => {
-    return db.query('SELECT * FROM reviews WHERE review_id = $1', [review_id]).then(({rows}) => {
+    const queryValuesArr = [review_id]
+    // const queryStr = `SELECT reviews.*, COUNT(comments.review_id) AS comment_count 
+    // FROM reviews 
+    // JOIN comments
+    // ON reviews.review_id = comments.review_id
+    // WHERE reviews.review_id = $${queryValuesArr.length}
+    // GROUP BY reviews.review_id;`;
+
+    const queryStr = `SELECT reviews.review_id, reviews.title, reviews.review_body, reviews.designer, reviews.review_img_url, reviews.votes, reviews.category, reviews.owner, reviews.created_at, COUNT(comments.review_id) AS comment_count 
+    FROM reviews 
+    JOIN comments
+    ON reviews.review_id = comments.review_id
+    WHERE reviews.review_id = $${queryValuesArr.length}
+    GROUP BY reviews.review_id;`;
+
+
+
+    // const queryStr = `SELECT * FROM reviews WHERE review_id = $${queryValuesArr.length}`;
+
+    return db.query(queryStr, queryValuesArr).then(({rows}) => {
         if (rows.length === 0){
             return Promise.reject({status: 404, msg: 'Review not found'})
         } else {
+            rows[0].comment_count = parseInt(rows[0].comment_count)
             return rows[0];
         }
     })
