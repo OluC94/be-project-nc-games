22d79ad7257f6fc2fd0401a6c97aaf5d5353ae21
OluC94 <coker.olu94@gmail.com>
2022-09-26 10:00:08 +0100

    allows cross origin resource sharing

--- api/app.js
+++ api/app.js
@@ -1,23 +1,40 @@
-const express = require('express');
-const { getCategories, getReviewByID, getUsers, patchReviewByID, getReviews, getCommentsByReviewID, postCommentByID, deleteCommentByCommentID, getApi } = require('./controllers/app.controllers');
-const { handleInternalServerErrors, handleBadPath, handleCustomErrors } = require('./controllers/errors.controllers');
+const express = require("express");
+const {
+  getCategories,
+  getReviewByID,
+  getUsers,
+  patchReviewByID,
+  getReviews,
+  getCommentsByReviewID,
+  postCommentByID,
+  deleteCommentByCommentID,
+  getApi,
+} = require("./controllers/app.controllers");
+const {
+  handleInternalServerErrors,
+  handleBadPath,
+  handleCustomErrors,
+} = require("./controllers/errors.controllers");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
-app.get('/api', getApi);
+app.get("/api", getApi);
 
-app.get('/api/categories', getCategories);
+app.get("/api/categories", getCategories);
 
-app.get('/api/reviews', getReviews);
-app.get('/api/reviews/:review_id', getReviewByID);
-app.patch('/api/reviews/:review_id', patchReviewByID);
-app.get('/api/reviews/:review_id/comments', getCommentsByReviewID)
-app.post('/api/reviews/:review_id/comments', postCommentByID);
+app.get("/api/reviews", getReviews);
+app.get("/api/reviews/:review_id", getReviewByID);
+app.patch("/api/reviews/:review_id", patchReviewByID);
+app.get("/api/reviews/:review_id/comments", getCommentsByReviewID);
+app.post("/api/reviews/:review_id/comments", postCommentByID);
 
-app.get('/api/users', getUsers);
+app.get("/api/users", getUsers);
 
-app.delete('/api/comments/:comment_id', deleteCommentByCommentID);
+app.delete("/api/comments/:comment_id", deleteCommentByCommentID);
 
 app.use(handleCustomErrors);
 app.use(handleBadPath);
